06f3295fe2350ed2e6b250b70091bdc5f7903db9
bernhard <bernhard.stoecker@recogizer.de>
2018-08-11 15:23:10 +0200

    Basic project structure

---
+++ lib/restarter_ex.ex
@@ -0,0 +1,18 @@
+defmodule RestarterEx do
+  @moduledoc """
+  Documentation for RestarterEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RestarterEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/restarter_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule RestarterExTest do
+  use ExUnit.Case
+  doctest RestarterEx
+
+  test "greets the world" do
+    assert RestarterEx.hello() == :world
+  end
+end
