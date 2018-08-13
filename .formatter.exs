8d5712fdffd0b8273ada01ed77a7c93a2a336a12
bernhard <bernhard.stoecker@recogizer.de>
2018-08-13 10:15:54 +0200

    Add formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,9 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    ".formatter.exs",
+    "mix.exs",
+    "{config,lib,priv,rel,test}/**/*.{ex,exs}"
+  ],
+  line_length: 80,
+  rename_deprecated_at: "1.7.0"
 ]
