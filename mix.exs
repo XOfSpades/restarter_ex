9dcbc079e06e78366be6e170dac00465cd07647e
bernhard <bernhard.stoecker@recogizer.de>
2018-11-27 17:23:28 +0100

    Add description

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule RestarterEx.MixProject do
       elixir: ">= 1.5.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      description: description,
+      package: package,
       deps: deps()
     ]
   end
@@ -16,6 +18,21 @@ defmodule RestarterEx.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    """
+    Library offering a supervisor with infinite restart functionality and backoff.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Bernhard Stöcker"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/xofspades/restarter_ex"}
     ]
   end
 
