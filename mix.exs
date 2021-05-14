5a8b06161edef73387caa58580b65fc0943b3d04
bernhard <bernhard.stoecker@recogizer.de>
2021-05-14 17:57:19 +0200

    Add ex_doc as dependency

--- mix.exs
+++ mix.exs
@@ -4,12 +4,12 @@ defmodule RestarterEx.MixProject do
   def project do
     [
       app: :restarter_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: ">= 1.5.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      description: description,
-      package: package,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -42,6 +42,6 @@ defmodule RestarterEx.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 end
