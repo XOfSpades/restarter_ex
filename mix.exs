e88c2fd70e93480e0e73d5545449fe57afe5a90b
bernhard <bernhard.stoecker@recogizer.de>
2021-05-19 10:42:35 +0200

    Set version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RestarterEx.MixProject do
   def project do
     [
       app: :restarter_ex,
-      version: "0.1.1",
+      version: "0.1.3",
       elixir: ">= 1.5.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
